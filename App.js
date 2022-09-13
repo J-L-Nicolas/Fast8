ea17e7266f5af97adcda857f811b70231ef4ff15
Jeremy LEMONY <J.lemony@outlook.fr>
2022-09-13 22:22:06 +0200

    init navigation and add views

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import Routes from './src/components/Routes'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Routes/>
   )
 }
 
