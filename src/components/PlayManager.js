f93737dc21b5bc4c57eb66374988e8072fc06efa
Jeremy LEMONY <J.lemony@outlook.fr>
2022-09-14 14:21:15 +0200

    add class

---
+++ src/components/PlayManager.js
@@ -0,0 +1,16 @@
+export default class Game {
+    constructor(name){
+        this.name = name
+
+        //start methodes
+        this.startGame()
+    }
+
+    startGame(){
+
+    }
+
+    popo(){
+        console.log(this.name)
+    }
+}
