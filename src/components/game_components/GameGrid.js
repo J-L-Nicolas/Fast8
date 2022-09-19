d7f56a3cbeb83c3916d1716b72fced1ff38960ef
Jeremy LEMONY <J.lemony@outlook.fr>
2022-09-19 14:55:22 +0200

    add components GameGrid

---
+++ src/components/game_components/GameGrid.js
@@ -0,0 +1,193 @@
+import React, {useEffect, useRef, useState} from 'react'
+import { StyleSheet, View, Animated, PanResponder } from 'react-native'
+
+const GameGrid = ({tableGame, changeTableGame, changeScore}) => {
+
+
+    console.log("update --: ", tableGame)
+
+    //init ref
+    const layoutInfos = useRef(null)
+    const layoutTableInfos = useRef([]).current
+    const pan = useRef(new Animated.ValueXY()).current;
+    const pan2 = useRef(new Animated.ValueXY()).current;
+    const colorSelected = useRef(new Animated.Value(8)).current
+    const colorSelectedHidde = useRef(new Animated.Value(0)).current
+
+    const panMove = (index) =>{
+        const panResponder = useRef(
+            PanResponder.create({
+                onStartShouldSetPanResponderCapture: (evt, gestureState) =>{
+                    pan.setValue({
+                        x: layoutTableInfos[index].x,
+                        y: layoutTableInfos[index].y
+                    });
+                    pan2.setValue({
+                        x: layoutTableInfos[index].x,
+                        y: layoutTableInfos[index].y
+                    });
+                    colorSelected.setValue(tableGame[index].id)
+                    colorSelectedHidde.setValue(1)
+                    return true
+                },
+              onMoveShouldSetPanResponder: () => true,
+              onPanResponderGrant: () => {
+                pan.setOffset({
+                  x: pan.x._value,
+                  y: pan.y._value
+                });
+              },
+              onPanResponderMove: Animated.event(
+                [
+                  null,
+                  { dx: pan.x, dy: pan.y }
+                ],
+                {useNativeDriver: false}
+              ),
+              onPanResponderRelease: (e, gestureState) => {
+                pan.flattenOffset();
+                afertMove(readMove(gestureState), index)
+
+                colorSelected.setValue(8)
+                colorSelectedHidde.setValue(0)
+              }
+            })
+        ).current;
+        return panResponder
+    }
+
+    const readMove = (gestureState) => {
+        pan.setValue({
+            x: -100,
+            y: -100
+        });
+        pan2.setValue({
+            x: -100,
+            y: -100
+        });
+        if( gestureState.dx > (layoutInfos.current.width * 0.6) && gestureState.dy < (layoutInfos.current.width * 0.5) && gestureState.dy > (layoutInfos.current.width * -0.5)){
+            return "right"
+        }
+        if( gestureState.dx < (layoutInfos.current.width * -0.6) && gestureState.dy < (layoutInfos.current.width * 0.5) && gestureState.dy > (layoutInfos.current.width * -0.5)){
+            return "left"
+        }
+        if( gestureState.dy > (layoutInfos.current.height * 0.6) && gestureState.dx < (layoutInfos.current.height * 0.5) && gestureState.dx > (layoutInfos.current.height * -0.5)){
+            return "down"
+        }
+        if( gestureState.dy < (layoutInfos.current.height * -0.6) && gestureState.dx < (layoutInfos.current.height * 0.5) && gestureState.dx > (layoutInfos.current.height * -0.5)){
+            return "up"
+        }
+    }
+
+    const afertMove = (dir, index) => {
+        console.log("Afertmove index: ", index )
+        console.log("table use: ", tableGame)
+
+        switch (dir) {
+            case "up":
+                    if ((index - 3) >= 0 && !tableGame[index - 3]){
+                        moveTable(index, index - 3)
+                    }
+                break;
+            
+            case "down":
+                    if ((index + 3) < tableGame.length && !tableGame[index + 3]){
+                        moveTable(index, index + 3)
+                    }
+                break;
+
+            case "right":
+                if ((index + 1) < tableGame.length && index !== 2  && index !== 5 && !tableGame[index + 1]){
+                    moveTable(index, index + 1)
+                }
+                break;
+
+            case "left":
+                if ((index - 1) >= 0 && index !== 3  && index !== 6 && !tableGame[index - 1]){
+                    moveTable(index, index - 1)
+                }
+
+            default:
+                break;
+        }
+    }
+
+    const moveTable = (indexIn, indexOut) =>{
+        let newTable = [...tableGame]
+        let save = tableGame[indexIn]
+
+        newTable[indexIn] = undefined
+        newTable[indexOut] = save
+
+        changeTableGame(newTable)
+        changeScore(0)
+    }
+
+    // interpolate aniated
+    const boxInterpolation =  colorSelected.interpolate({
+        inputRange: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        outputRange:["#ff0000" , "#ff0000", "#00c210", "#0927ae","#0927ae","#0927ae","#b86e00", "#ad0bd5", "#ffffff00"]
+    })
+        const boxInterpolationHidde =  colorSelectedHidde.interpolate({
+        inputRange: [0, 1],
+        outputRange:["#ffffff00" , "#515ba1"]
+    })
+
+    // render function
+    return(
+        <View style={styles.containerCube}>
+            {tableGame.map((cube, index)=>
+                cube ?
+                <View 
+                    style={[styles.cube, {backgroundColor: cube.color}]} 
+                    key={index}  
+                    onLayout={event => {
+                        const positionElement = event.nativeEvent.layout;
+                        if (index == 0){
+                            layoutInfos.current = positionElement;
+                        }
+                        layoutTableInfos[index] = positionElement;
+                    }}
+                    
+                    {...panMove(index).panHandlers} 
+                />
+                :
+                <View style={[styles.cube, {backgroundColor: "#ffffff00"}]} key={index}/>
+            )}
+            <Animated.View
+                style={[ styles.cubeHidde, {backgroundColor: boxInterpolationHidde,top: pan2.y , left: pan2.x} ]}
+            />
+            <Animated.View
+                style={[ styles.cubeMove, {backgroundColor: boxInterpolation,top: pan.y , left: pan.x} ]}
+            />
+        </View>
+    )
+}
+
+export default GameGrid
+
+const styles = StyleSheet.create({
+    containerCube:{
+        position:'relative',
+        width: 315,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    cube:{
+        width: 100,
+        height: 100,
+        marginBottom: 5,
+    },
+    cubeHidde:{
+        width: 100,
+        height: 100,
+        position: 'absolute'
+    },
+    cubeMove:{
+        width: 100,
+        height: 100,
+        position: 'absolute'
+    },
+})
--- src/views/Game.js
+++ src/views/Game.js
@@ -1,5 +1,5 @@
 import React, {useState, useRef, useEffect} from 'react'
-import { StyleSheet, View, PanResponder, Animated, Vibration, Image } from 'react-native'
+import { StyleSheet, View, Vibration, Image } from 'react-native'
 import {useStoreState} from 'easy-peasy'
 import PlayManager from '../components/PlayManager'
 
@@ -8,6 +8,7 @@ import BtnReloadRef from '../components/game_components/BtnReloadRef'
 import Infos from '../components/game_components/Infos'
 import SnapSlider from '../components/game_components/SnapSlider'
 import TableRef from '../components/game_components/TableRef'
+import GameGrid from '../components/game_components/GameGrid'
 
 const Game = () => {
 
@@ -20,120 +21,7 @@ const Game = () => {
     const [dataInfos, setdataInfos] = useState({score: 0, move: 0, oldMoves: []})
 
     //init ref
-    const layoutInfos = useRef(null)
-    const layoutTableInfos = useRef([]).current
-    const pan = useRef(new Animated.ValueXY()).current;
-    const pan2 = useRef(new Animated.ValueXY()).current;
-    const colorSelected = useRef(new Animated.Value(8)).current
-    const colorSelectedHidde = useRef(new Animated.Value(0)).current
     const levelPoint = useRef(2)
-
-    const panMove = (index) =>{
-        const panResponder = useRef(
-            PanResponder.create({
-                onStartShouldSetPanResponderCapture: (evt, gestureState) =>{
-                    pan.setValue({
-                        x: layoutTableInfos[index].x,
-                        y: layoutTableInfos[index].y
-                    });
-                    pan2.setValue({
-                        x: layoutTableInfos[index].x,
-                        y: layoutTableInfos[index].y
-                    });
-                    colorSelected.setValue(cubeTable[index].id)
-                    colorSelectedHidde.setValue(1)
-                    return true
-                },
-              onMoveShouldSetPanResponder: () => true,
-              onPanResponderGrant: () => {
-                pan.setOffset({
-                  x: pan.x._value,
-                  y: pan.y._value
-                });
-              },
-              onPanResponderMove: Animated.event(
-                [
-                  null,
-                  { dx: pan.x, dy: pan.y }
-                ],
-                {useNativeDriver: false}
-              ),
-              onPanResponderRelease: (e, gestureState) => {
-                pan.flattenOffset();
-                afertMove(readMove(gestureState), index)
-
-                colorSelected.setValue(8)
-                colorSelectedHidde.setValue(0)
-              }
-            })
-        ).current;
-        return panResponder
-    }
-
-    const readMove = (gestureState) => {
-        pan.setValue({
-            x: -100,
-            y: -100
-        });
-        pan2.setValue({
-            x: -100,
-            y: -100
-        });
-        if( gestureState.dx > (layoutInfos.current.width * 0.6) && gestureState.dy < (layoutInfos.current.width * 0.5) && gestureState.dy > (layoutInfos.current.width * -0.5)){
-            return "right"
-        }
-        if( gestureState.dx < (layoutInfos.current.width * -0.6) && gestureState.dy < (layoutInfos.current.width * 0.5) && gestureState.dy > (layoutInfos.current.width * -0.5)){
-            return "left"
-        }
-        if( gestureState.dy > (layoutInfos.current.height * 0.6) && gestureState.dx < (layoutInfos.current.height * 0.5) && gestureState.dx > (layoutInfos.current.height * -0.5)){
-            return "down"
-        }
-        if( gestureState.dy < (layoutInfos.current.height * -0.6) && gestureState.dx < (layoutInfos.current.height * 0.5) && gestureState.dx > (layoutInfos.current.height * -0.5)){
-            return "up"
-        }
-    }
-
-    const afertMove = (dir, index) => {
-        switch (dir) {
-            case "up":
-                    if ((index - 3) >= 0 && !cubeTable[index - 3]){
-                        moveTable(index, index - 3)
-                    }
-                break;
-            
-            case "down":
-                    if ((index + 3) < cubeTable.length && !cubeTable[index + 3]){
-                        moveTable(index, index + 3)
-                    }
-                break;
-
-            case "right":
-                if ((index + 1) < cubeTable.length && index !== 2  && index !== 5 && !cubeTable[index + 1]){
-                    moveTable(index, index + 1)
-                }
-                break;
-
-            case "left":
-                if ((index - 1) >= 0 && index !== 3  && index !== 6 && !cubeTable[index - 1]){
-                    moveTable(index, index - 1)
-                }
-
-            default:
-                break;
-        }
-    }
-
-    const moveTable = (indexIn, indexOut) =>{
-        let newTable = [...cubeTable]
-        let save = cubeTable[indexIn]
-
-        newTable[indexIn] = undefined
-        newTable[indexOut] = save
-
-        setcubeTable(newTable)
-        updateScore(0)
-
-    }
 
     //updateRef
     const updateRef = (type = false) => {
@@ -147,8 +35,10 @@ const Game = () => {
       const result = PlayManager.comparTabs(cubeTable, refTable, "idColor")
       if (result) {
         updateScore(1)
+        refGame.force
         updateRef()
         Vibration.vibrate(50)
+
       }
     }, [cubeTable])
 
@@ -167,50 +57,6 @@ const Game = () => {
         setdataInfos(mewData)
     }
     
-    // display table
-    const DisplayTable = () => {
-
-        // interpolate aniated
-        const boxInterpolation =  colorSelected.interpolate({
-            inputRange: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            outputRange:["#ff0000" , "#ff0000", "#00c210", "#0927ae","#0927ae","#0927ae","#b86e00", "#ad0bd5", "#ffffff00"]
-        })
-        const boxInterpolationHidde =  colorSelectedHidde.interpolate({
-            inputRange: [0, 1],
-            outputRange:["#ffffff00" , "#515ba1"]
-        })
-
-        // render function
-        return(
-            <View style={styles.containerCube}>
-                {cubeTable.map((cube, index)=>
-                    cube ?
-                    <View 
-                        style={[styles.cube, {backgroundColor: cube.color}]} 
-                        key={index}  
-                        onLayout={event => {
-                            const positionElement = event.nativeEvent.layout;
-                            if (index == 0){
-                                layoutInfos.current = positionElement;
-                            }
-                            layoutTableInfos[index] = positionElement;
-                        }}
-                        
-                         {...panMove(index).panHandlers} 
-                    />
-                    :
-                    <View style={[styles.cube, {backgroundColor: "#ffffff00"}]} key={index}/>
-                )}
-                <Animated.View
-                    style={[ styles.cubeHidde, {backgroundColor: boxInterpolationHidde,top: pan2.y , left: pan2.x} ]}
-                />
-                <Animated.View
-                    style={[ styles.cubeMove, {backgroundColor: boxInterpolation,top: pan.y , left: pan.x} ]}
-                />
-            </View>
-        )
-    }
-
     // change bar animate
     const changeSnap = (level) =>{
         levelPoint.current = level
@@ -224,14 +70,14 @@ const Game = () => {
                 style={{ resizeMode: "contain", height: "100%", position: 'absolute', top: 0}}
             />
             <View style={styles.containerHeader}>
-                    <View style={styles.boxInHeader}>
-                        <TableRef table={refTable}/>
-                        <BtnReloadRef refUpdate={updateRef} />
-                    </View>
-                    <SnapSlider snapValue={changeSnap}/>
+                <View style={styles.boxInHeader}>
+                    <TableRef table={refTable}/>
+                    <BtnReloadRef refUpdate={updateRef} />
+                </View>
+                <SnapSlider snapValue={changeSnap}/>
             </View>
-                <View style={styles.containerBody}>
-                <DisplayTable/>
+            <View style={styles.containerBody}>
+                <GameGrid tableGame={cubeTable} key={cubeTable} changeTableGame={setcubeTable} changeScore={updateScore}/>
                 <Infos infos={dataInfos}/>
             </View>
         </View>
@@ -271,29 +117,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems:'center',
         paddingTop: 25,
-    },
-    //stye ---------
-    containerCube:{
-        position:'relative',
-        width: 315,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-    },
-    cube:{
-        width: 100,
-        height: 100,
-        marginBottom: 5,
-    },
-    cubeMove:{
-        width: 100,
-        height: 100,
-        position: 'absolute'
-    },
-    cubeHidde:{
-        width: 100,
-        height: 100,
-        position: 'absolute'
     },
 })
