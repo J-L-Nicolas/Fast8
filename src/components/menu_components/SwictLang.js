ee1785574659e5b2adcc3e6a1c9ba1dffb66cb07
Jeremy LEMONY <J.lemony@outlook.fr>
2022-09-23 15:33:09 +0200

    build animation switch lang

--- src/components/menu_components/SwictLang.js
+++ src/components/menu_components/SwictLang.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity , Image, View} from 'react-native'
 import {useStoreState, useStoreActions} from 'easy-peasy'
 
 const SwictLang = () => {
@@ -11,6 +11,16 @@ const SwictLang = () => {
   return (
     <TouchableOpacity onPress={toggleLang}>
         <Text>{IdLang == "en" ?  "Français" : "English" }</Text>
+        <View style={styles.boxStyle}>
+          <Image
+            source={require("../../assets/img/flag_fr.png")}
+            style={styles.ImageFlag1}
+          />
+          <Image
+            source={require("../../assets/img/flag_en.png")}
+            style={styles.ImageFlag2}
+          />
+        </View>
     </TouchableOpacity>
   )
 }
@@ -19,5 +29,27 @@ export default SwictLang
 
 //Style
 const styles = StyleSheet.create({
-
+  boxStyle:{
+    position:"relative",
+    justifyContent: "center",
+    alignItems: 'center',
+    flexDirection: "row",
+  },
+  ImageFlag1:{
+    position: 'absolute',
+    width: 65,
+    height: 50,
+    resizeMode: 'cover',
+    transform:  [{ translateX: 30 }, {translateY: 20}],
+    elevation: 2,
+    zIndex: 5,
+  },
+  ImageFlag2:{
+    position: 'absolute',
+    width: 65,
+    height: 50,
+    resizeMode: 'cover',
+    elevation: 1,
+    zIndex: 1,
+  }
 })
